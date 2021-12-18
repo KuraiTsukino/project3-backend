6474a6bb08cd635a48d2195881bff8e19699c86b
Kurai Tsukino <sandym3000@hotmail.com>
2021-12-18 13:03:25 -0600

    crear un lugar

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -1,6 +1,48 @@
 // 1. Importación de modelo.
 const Place = require("./../models/Place")
 
+exports.create = async (req, res) => {
+
+    // Del formulario, creamos variables y asignamos valores.
+    const {
+        country,
+        name,
+        type,
+        schedule,
+        price,
+        image,
+        description,
+        map
+    } = req.body
+
+    // Crear un lugar en base de datos
+    try {
+        const newPlace = await Place.create({
+            country,
+            name,
+            type,
+            schedule,
+            price,
+            image,
+            description,
+            map
+        })
+        // Devolver una respuesta en un formato Json
+        res.json ({
+            msg: "Lugar creado con éxito",
+            data: newPlace
+        })
+
+    } catch (error) {
+        
+        res.status(500).json({
+            msg: "Hubo un error creando el lugar",
+            error: error
+        })
+        console.log(error)
+    }
+}
+
 exports.readAll = async (req, res) => {
 
     try {
@@ -36,6 +78,71 @@ exports.readOne = async (req, res) => {
         
         res.status(500).json({
             msg: "Hubo un error obteniendo los datos",
+            error: error
+        })
+    }
+}
+
+exports.edit = async (req, res) => {
+
+    const { id } = req.params
+
+    // Obteniendo datos del formulario
+    const {
+        country,
+        name,
+        type,
+        schedule,
+        price,
+        image,
+        description,
+        map
+    }= req.body
+
+    try {
+        const updatedPlace = await Place.findByIdAndUpdate(
+            id,
+            {
+                country,
+                name,
+                type,
+                schedule,
+                price,
+                image,
+                description,
+                map
+            },
+            {new:true}
+        )
+        res.json({
+            msg: "Lugar actualizado con éxito",
+            data: updatedGuitar
+        })
+
+    } catch (error) {
+        
+        res.status(500).json({
+            msg: "Hubo un error con la actualización del lugar",
+            error: error
+        })
+    }
+}
+
+exports.delete = async (req, res) => {
+    
+    const { id } = req.params
+
+    try {
+        const deletedPlace = await Place.findByIdAndDelete({_id: id})
+
+        res.json({
+            msg: "Lugar borrado con éxito",
+            data: deletedPlace
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            msg: "Hubo un error borrando el lugar",
             error: error
         })
     }
--- routes/places.js
+++ routes/places.js
@@ -7,10 +7,20 @@ const placeController = require("./../controllers/placeController")
 
 // 2. Ruteo. Router
 
-// Leer los países.
+// Crear un lugar.
+router.post("/create", placeController.create)
+
+// Leer todos los lugares.
 router.get("/readall", placeController.readAll)
 
+// Leer un lugar.
 router.get("/readone/:id", placeController.readOne)
+
+// Actualizar un lugar.
+router.put("/edit/:id", placeController.edit)
+
+// Borrar un lugar.
+router.delete("/delete/:id", placeController.delete)
 
 // 3. Exportaciones
 module.exports = router
